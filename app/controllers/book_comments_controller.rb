6812453272ceefb9a7fbde79848167f56e647704
sukepio <naoto927sukena@gmail.com>
2021-01-05 10:49:39 +0000

    [Add] コメント機能

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,9 +1,25 @@
 class BookCommentsController < ApplicationController
   
   def create
+    @book = Book.find(params[:book_id])
+    @book_comment = current_user.book_comments.new(book_comment_params)
+    @book_comment.book_id = @book.id
+    if @book_comment.save
+      redirect_to request.referrer
+    else
+      render "books/show"
+    end
   end
   
-  def destory
+  def destroy
+    BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
+    redirect_to request.referrer
+  end
+  
+  private
+  
+  def book_comment_params
+    params.require(:book_comment).permit(:comment)
   end
   
 end
