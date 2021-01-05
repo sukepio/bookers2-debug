7b958a14d953fb0f15533750451e835b5ad59ac7
sukepio <naoto927sukena@gmail.com>
2021-01-05 13:23:49 +0000

    [Start] フォロー機能

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_05_073938) do
+ActiveRecord::Schema.define(version: 2021_01_05_131458) do
 
   create_table "book_comments", force: :cascade do |t|
     t.text "comment"
@@ -33,6 +33,14 @@ ActiveRecord::Schema.define(version: 2021_01_05_073938) do
     t.integer "book_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
   end
 
   create_table "users", force: :cascade do |t|
