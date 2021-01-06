b82c45aad3fff0624d54fbdad1d46a9d6d3336f6
sukepio <naoto927sukena@gmail.com>
2021-01-06 12:36:46 +0000

    [Finish] コメント機能非同期化

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -6,15 +6,16 @@ class BookCommentsController < ApplicationController
     @book_comment = current_user.book_comments.new(book_comment_params)
     @book_comment.book_id = @book.id
     if @book_comment.save
-      redirect_to book_path(@book)
+      render :create
     else
       render "books/show"
     end
   end
   
   def destroy
-    BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-    redirect_to request.referrer
+    @book = Book.find(params[:book_id])
+    @book_comment = @book.book_comments.find(params[:id])
+    @book_comment.destroy
   end
   
   private
