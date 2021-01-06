24136cf5511d3f5908556cd608061168c1deebb4
sukepio <naoto927sukena@gmail.com>
2021-01-06 03:17:52 +0000

    [add] 検索フォーム

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,2 +1,36 @@
 class SearchesController < ApplicationController
+  
+  def search
+    @model = params["model"]
+    @content = params["content"]
+    @method = params["method"]
+    @records = search_for(@model, @content, @method)
+  end
+  
+  private
+  
+  def search_for(model, content, method)
+    if model == 'user'
+      if method == 'perfect'
+        User.where(name: content)
+      elsif method == '前方一致'
+        User.where('name LIKE ?', content+'%')
+      elsif method == '後方一致'
+        User.where('name LIKE ?', '%'+content)
+      else
+        User.where('name LIKE ?', '%'+content+'%')
+      end
+    elsif model == 'book'
+      if method == 'perfect'
+        Book.where(body: content)
+      elsif method == '前方一致'
+        Book.where('body LIKE ?', content+'%')
+      elsif method == '後方一致'
+        Book.where('body LIKE ?', '%'+content)
+      else
+        Book.where('body LIKE ?', '%'+content+'%')
+      end
+    end
+  end
+  
 end
