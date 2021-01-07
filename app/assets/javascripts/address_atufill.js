802b52ca7bb74f7f24c90065410872fa0888c5ae
sukepio <naoto927sukena@gmail.com>
2021-01-07 01:49:46 +0000

    [Finish] 住所自動入力

---
+++ app/assets/javascripts/address_atufill.js
@@ -0,0 +1,12 @@
+$(function() {
+  return $('#user_postcode').jpostal({
+  postcode: ['#user_postcode'],
+  address: {
+    '#user_prefecture_code': '%3',
+    '#user_address_city': '%4%5',
+    '#user_address_street': '%6%7',
+  },
+  });
+});
+
+  
--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,7 @@ class User < ApplicationRecord
   validates :name, length: {maximum: 20, minimum: 2}, uniqueness: true
   validates :introduction, length: {maximum: 50}
   
+  # フォロー機能
   has_many :follower, class_name: 'Relationship', foreign_key: 'follower_id', dependent: :destroy
   has_many :following_user, through: :follower, source: :followed
   has_many :followed, class_name: 'Relationship', foreign_key: 'followed_id', dependent: :destroy
@@ -28,4 +29,16 @@ class User < ApplicationRecord
   def following?(user)
     following_user.include?(user)
   end
+  
+  # 住所自動入力
+  include JpPrefecture
+    jp_prefecture :prefecture_code
+    
+  def prefecture_name
+    JpPrefecture::Prefecture.find(code: prefecture_code).try(:name)
+  end
+  
+  def prefecture_name=(prefecture_name)
+    self.prefecture_code = JpPrefecture::Prefecture.find(name: prefecture_name).code
+  end  
 end
