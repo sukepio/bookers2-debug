f730a79718ba9935eba05d893f6f946a16012da5
sukepio <naoto927sukena@gmail.com>
2021-01-29 02:13:33 +0000

    finish adding google map

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -3,5 +3,16 @@ class HomesController < ApplicationController
   end
 
   def about
+    
+  end
+  
+  def map 
+    
+    results = Geocoder.search(params[:address])
+    @latlng = results.first.coordinates
+    
+    respond_to do |format|
+      format.js
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,31 +1,32 @@
 Rails.application.routes.draw do
   root 'homes#top'
   get 'home/about' => 'homes#about'
+  get '/map_request', to: 'homes#map', as: 'map_request'
   devise_for :users, :controllers => {
     :sessions => 'users/sessions',
     :registrations => 'users/registrations',
   }
-  
-  resources :users,only: [:show,:index,:edit,:update] do 
-    member do 
+
+  resources :users,only: [:show,:index,:edit,:update] do
+    member do
       get :following, :follower
     end
     collection do
       post :confirm
     end
   end
-  
-  resources :books do 
+
+  resources :books do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
-  
+
   post "create/:id" => "relationships#create", as: 'follow'
   post "destroy/:id" => "relationships#destroy", as: 'unfollow'
-  
+
   get "search" => "searches#search"
-  
+
   get "chat/:id" => "chats#show", as: 'chat'
   resources :chats, only: [:create]
-  
+
 end
